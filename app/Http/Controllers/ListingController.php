bc58fcdc0341b1ff060d47d261e521cc392c0b51
MuneebAccrual <muneeb.rehman@accrualhub.com>
2024-03-14 10:39:10 +0500

    CK Editor update code added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -135,6 +135,113 @@ class ListingController extends Controller
         }
     }
 
+
+    public function updateListingUser(Request $request){
+        $validator = Validator::make($request->all(),[
+            'company_name' => 'required|string',
+            'company_categories' => 'required',
+            'company_tagline' => 'required',
+            'short_description' => 'required|string',
+            // 'company_logo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $validations = ['company_name', 'company_categories', 'company_tagline', 'short_description'];
+        $errors = [];
+        foreach($validations as $val){
+            foreach($validator->errors()->get($val) as $error){
+                $errors[] = $error;
+            }
+        }
+        if(!empty($errors)){
+            return response()->json(["success"=>false, "error"=>$errors, "status"=>400], 400);
+        }
+
+        try{
+            // if ($request->file('company_logo')) {
+            //     $file = $request->file('company_logo');
+            //     $fileName = time() .'_' .  rand() . '.' . $file->getClientOriginalExtension();
+            //     $destinationPath = public_path('assets/listing_images'); // Use public_path() to get the physical file system path
+            //     $file->move($destinationPath, $fileName);
+            // }
+            
+            // Saving Short Description of Editor
+            $designDocument  = $request->short_description;
+            $dom = new \DOMDocument();
+            libxml_use_internal_errors(true);
+            $dom->loadHtml($designDocument, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+            libxml_use_internal_errors(false);
+            $images = $dom->getElementsByTagName('img');
+
+            foreach($images as $item => $image){
+                $data = $image->getAttribute("src");
+                $styles = $image->getAttribute("style");
+                $image_explode = explode(';', $data);
+                if(count($image_explode) == 2){
+                    list($type, $data) = [$image_explode[0] , $image_explode[1]];
+                    list(, $data)      = explode(',', $data);
+                }else{
+                    continue;
+                }
+                // list($type, $data) = explode(';', $data);
+                // list(, $data)      = explode(',', $data);
+                $imgeData = base64_decode($data);
+                $image_name= time().$item.'.png';
+                $path = public_path().'/assets/listing_description_image/' . $image_name;
+                file_put_contents($path, $imgeData);
+                $image->removeAttribute('src');
+                $image->setAttribute('src', asset('assets/listing_description_image/' . $image_name));
+                $image->setAttribute('width' , $styles);
+                $image->setAttribute('class', 'ck-image');
+                $image->removeAttribute("style");
+            }
+            $content = $dom->saveHTML();
+            
+            $user_id = Auth::user()->id;
+            $listingId = $request->id;
+            $listing = Listing::where('id', $listingId)->first(); // 0 means draft    
+            if(!empty($listing) || isset($listing)){
+                $listing->update([
+                    'company_name' => $request->company_name,
+                    'short_description' => $content,
+                    'company_tagline' => $request->company_tagline,
+                    // 'company_logo' => $fileName,
+                    'status' => '1', // 1 means pending (needs approval from Admin and it should change to 2)
+                ]);
+                // add categories to the user
+                $categories = json_decode($request->company_categories);
+                ListingCategory::where('listing_id', $listingId)->delete();
+                foreach($categories as $category){
+                    $categoryInsert = new ListingCategory();
+                    $categoryInsert->listing_id = $listingId;
+                    $categoryInsert->category_id = $category;
+                    $categoryInsert->save();
+                }
+                // adding deals
+                $deals = json_decode($request->deals);
+                if(isset($deals)){
+                    foreach($deals as $deal){
+                        $insertDeal = new Deal();
+                        $insertDeal->listing_id = $listingId;
+                        $insertDeal->deal_name = $deal->deal_name;
+                        $insertDeal->currency = $deal->currency;
+                        $insertDeal->discount_price = $deal->discount_price;
+                        $insertDeal->actual_price = $deal->actual_price;
+                        $insertDeal->billing_interval = $deal->billing_interval;
+                        $insertDeal->type = $deal->type;
+                        $insertDeal->coupon_code = $deal->coupon_code;
+                        $insertDeal->link = $deal->link;
+                        $insertDeal->save();
+                    }
+                }
+                return response()->json(["success"=>true, "msg"=>"Listing updated successfully and is under review", "status"=>200], 200);
+            }else{
+                return response()->json(["success"=>false, "msg"=>"Listing has already been created for the selected tool", "status"=>400], 400);
+            }
+        }catch(\Exception $e){
+            return response()->json(["success"=>false, "msg"=>"Something Went Wrong", "error"=>$e->getMessage(), "line"=>$e->getLine()], 400);
+        }
+    }
+
     // for deleting Listing 
     public function deleteListingUser(Request $request){
         try{
